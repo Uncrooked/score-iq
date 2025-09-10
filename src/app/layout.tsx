0efb47fce1b420c853af90427b9e131b9c5f447e
Léo Murail <leo.murai85@gmail.com>
2025-09-10 11:18:58 +0200

    dev : change app root code

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+export default function Layout({children}:{children:React.ReactNode}){
+    return(
+        <html lang="en">
+            <head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import "./global.css";
+
+export default function Page() {
+  return (
+    <h1>Hello</h1>
+  );
+}
