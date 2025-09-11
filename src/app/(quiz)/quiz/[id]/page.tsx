84115e44b100b28d96a89c1b476c234b4479f43b
Léo Murail <leo.murai85@gmail.com>
2025-09-11 16:12:47 +0200

    feat(connect-page) : init

--- src/app/(quiz)/quiz/[id]/page.tsx
+++ src/app/(quiz)/quiz/[id]/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { HomeIcon } from "lucide-react";
 import QuizWrapper from "@/src/components/ui/QuizWrapper/QuizWrapper";
+import { getUserName } from "@/src/actions/userName";
 // import { prisma } from "@db";
 
 export default async function Page({params}:{
@@ -10,11 +11,16 @@ export default async function Page({params}:{
 }){
     const { id } = await params;
 
+    const userCookie = await getUserName();
+
     return(
         <section id="quiz-page">
-            <Link href="/">
-                <Button variant="outline" className="home-btn"><HomeIcon/> Accueil</Button>
-            </Link>
+            <div id="head">
+                <Link href="/">
+                    <Button variant="outline" className="home-btn"><HomeIcon/> Accueil</Button>
+                </Link>
+                <p id="user-name">{userCookie?.value}</p>
+            </div>
             <QuizWrapper id={id} />
             <div className="tag">1</div>
         </section>
